97b7ae36c5c48e600055e69aa7fe0efa66bbdf99
Pavel Zaitsau <zaitsaup@gmail.com>
2024-08-09 19:35:56 +0300

    Initial commit

---
+++ src/main/java/org/clevertech/git/pz/App.java
@@ -0,0 +1,13 @@
+package org.clevertech.git.pz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

de67a0ee911a6d854543262d40b7748210f40e88
Pavel Zaitsau <zaitsaup@gmail.com>
2024-08-09 22:49:27 +0300

    Only bug fix in App.main()

--- src/main/java/org/clevertech/git/pz/App.java
+++ src/main/java/org/clevertech/git/pz/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Car!" );
+        System.out.println( "It's Car application!" );
     }
 }

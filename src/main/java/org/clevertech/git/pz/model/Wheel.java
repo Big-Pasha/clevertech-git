39e01abddf426dc3adea0a21b626bf04d251616e
Pavel Zaitsau <zaitsaup@gmail.com>
2024-08-09 23:03:45 +0300

    Added TOURING ProtectorType for Wheel class

--- src/main/java/org/clevertech/git/pz/model/Wheel.java
+++ src/main/java/org/clevertech/git/pz/model/Wheel.java
@@ -3,7 +3,7 @@ package org.clevertech.git.pz.model;
 public class Wheel {
 
     enum ProtectorType {
-        SUMMER, All_SEASON
+        SUMMER, All_SEASON, TOURING
     }
 
     private Double radius;

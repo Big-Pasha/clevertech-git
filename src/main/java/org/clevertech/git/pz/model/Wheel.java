9dc6a35f0be723f5f3004b1c5d44ec116f93cd10
Pavel Zaitsau <zaitsaup@gmail.com>
2024-08-09 22:29:06 +0300

    Added ProtectorType enum in class Wheel and the same named field

--- src/main/java/org/clevertech/git/pz/model/Wheel.java
+++ src/main/java/org/clevertech/git/pz/model/Wheel.java
@@ -2,7 +2,13 @@ package org.clevertech.git.pz.model;
 
 public class Wheel {
 
+    enum ProtectorType {
+        SUMMER, All_SEASON
+    }
+
     private Double radius;
 
     private Double pressureBar;
+
+    private ProtectorType protectorType;
 }
